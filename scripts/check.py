e3b19d30405abf46ca26f13f412d6f7103aec0e0
NathanPERIER <periernathan2000@gmail.com>
2025-06-11 01:24:08 +0200

    Added script to check the data integrity

---
+++ scripts/check.py
@@ -0,0 +1,130 @@
+
+import sys
+
+from pokemon_data import load_groups, pkmn_group
+
+
+class error_logger :
+    def __init__(self):
+        self.has_error: bool = False
+        self.print_num: bool = True
+        self.num: int = 0
+
+    def start_group(self, number: int):
+        self.num = number
+        self.print_num = False
+
+    def error(self, message: str):
+        self.has_error = True
+        if not self.print_num :
+            print(f"===== Group #{self.num:04d} ==========================")
+            self.print_num = True
+        print(f"ERR: {message}")
+    
+    def ok(self) -> bool :
+        return not self.has_error
+
+
+def main() -> int :
+    try:
+        pokemon_data = load_groups()
+        print('Schema validation OK')
+    except Exception as e:
+        print(f"Data does not match the provided schema: {e}")
+        return 1
+
+    logger = error_logger()
+
+    last_number: int | None = None
+    all_groups: dict[int, pkmn_group] = {}
+    for group in pokemon_data:
+        logger.start_group(group.number)
+        if group.number in all_groups :
+            logger.error(f"Group {group.number} was found several times)")
+        if last_number is not None and last_number+1 != group.number :
+            logger.error(f"Group {group.number} is misplaced (previous is {last_number})")
+        last_number = group.number
+        all_groups[group.number] = group
+        known_variants: list[str | None] = []
+        for form in group.forms :
+            if form.variant is None and form.derives is not None :
+                logger.error('Default form cannot be derived from another form')
+            if form.variant in known_variants :
+                logger.error(f"Duplicate variant {form.variant}")
+            known_variants.append(form.variant)
+        if None not in known_variants and group.common_names is None :
+            logger.error('Groups without a default variant must define common names')
+        if any(x is None for x in known_variants) :
+            if group.forms[0].variant is not None :
+                logger.error('Null variant is not the first entry in the form list')
+        else:
+            if len(group.forms) < 2 :
+                logger.error(f"Group with only one form should not have a variant (found {group.forms[0].variant})")
+            if group.forms[0].derives is not None :
+                logger.error(f"First form in group (with variant {group.forms[0].variant}) cannot derive other forms")
+            if group.forms[0].is_temporary() :
+                logger.error(f"First form in group (with variant {group.forms[0].variant}) cannot be temporary")
+    for group in pokemon_data:
+        logger.start_group(group.number)
+        pre_evolution: pkmn_group | None = None
+        if group.evolves_from is not None :
+            if group.evolves_from in all_groups :
+                pre_evolution = all_groups[group.evolves_from]
+            else:
+                logger.error(f"Group {group.number} evolves from unknown group {group.evolves_from}")
+        for form in group.forms :
+            # TODO use a list here
+            if form.evolution_variant is not None :
+                if form.derives is not None :
+                    logger.error(f"Found evolution variant {form.evolution_variant} for derived group")
+                if group.evolves_from is None :
+                    logger.error(f"Found evolution variant {form.evolution_variant} for non-evolving group")
+                    continue
+                if pre_evolution is not None :
+                    pre_form = pre_evolution.find_form(form.evolution_variant)
+                    if pre_form is None :
+                        logger.error(f"Evolution variant {form.evolution_variant} does not exist in pre-evolution group {pre_evolution.number}")
+                    if pre_form.derives is not None :
+                        logger.error(f"Evolution variant {form.evolution_variant} in pre-evolution group {pre_evolution.number} refers to a derived form")
+            elif pre_evolution is not None :
+                pre_form = pre_evolution.find_form(form.variant)
+                if pre_form is None:
+                    if form.variant is None :
+                        logger.error(f"Pre-evolution group {pre_evolution.number} does not have a default variant to evolve")
+                        continue
+                    else:
+                        pre_form = pre_evolution.find_form(None)
+                        if pre_form is None :
+                            logger.error(f"Pre-evolution group {pre_evolution.number} does not have a {form.variant} variant to evolve, nor a default variant")
+                            continue
+                if pre_form.derives is not None :
+                    if pre_form.variant is None :
+                        logger.error(f"Default evolution variant in pre-evolution group {pre_evolution.number} refers to a derived form")
+                    else:
+                        logger.error(f"Evolution variant {pre_form.variant} in pre-evolution group {pre_evolution.number} refers to a derived form")
+    for group in pokemon_data :
+        logger.start_group(group.number)
+        for form in group.forms :
+            if form.derives is not None :
+                for derived_variant in form.derives.from_variants :
+                    if derived_variant == form.variant :
+                        logger.error(f"Variant {form.variant} derives from itself")
+                        continue
+                    derived_form = group.find_form(derived_variant)
+                    if derived_form is None :
+                        if derived_variant is None :
+                            logger.error(f"Variant {form.variant} derives from non-existing default variant")
+                        else:
+                            logger.error(f"Variant {form.variant} derives from non-existing variant {derived_variant}")
+                        continue
+                    if derived_form.is_temporary() and not form.is_temporary() :
+                        logger.error(f"Non-temporary variant {form.variant} derives from temporary variant {derived_variant}")
+
+    if logger.ok() :
+        print(f"Checked {len(pokemon_data)} groups => all OK")
+        return 0
+    return 1
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ scripts/pokemon_data.py
@@ -0,0 +1,62 @@
+
+import json
+
+import jsonschema
+
+
+def load_json(path: str):
+    with open(path, 'r') as f:
+        return json.load(f)
+
+
+class pkmn_names:
+    def __init__(self, data):
+        self.fr: str = data['fr']
+        self.en: str = data['en']
+
+class pkmn_links:
+    def __init__(self, data):
+        self.bulbapedia: str = data['bulbapedia']
+        self.pokepedia: str = data['pokepedia']
+
+class pkmn_derivation:
+    def __init__(self, data):
+        self.from_variants: list[str | None] = data['from']
+        self.temporary: bool = data['temporary']
+
+class pkmn_form:
+    def __init__(self, data):
+        self.names = pkmn_names(data['names'])
+        self.links = pkmn_links(data['links'])
+        self.types: list[str] = data['types']
+        self.gen: int = data['gen']
+        self.variant: str | None = data['variant'] if 'variant' in data else None
+        self.evolution_variant: str | None = data['evolution_variant'] if 'evolution_variant' in data else None
+        self.gender_variant: bool = data['gender_variant'] if 'gender_variant' in data else False
+        self.derives: pkmn_derivation | None = None
+        if 'derives' in data and data['derives'] is not None :
+            self.derives = pkmn_derivation(data['derives'])
+    
+    def is_temporary(self) -> bool :
+        return self.derives is not None and self.derives.temporary
+
+class pkmn_group:
+    def __init__(self, data):
+        self.number: int = data['number']
+        self.common_names: pkmn_names | None = None
+        if 'common_names' in data and data['common_names'] is not None :
+            self.common_names = pkmn_names(data['common_names'])
+        self.evolves_from: int | None = data['evolves_from']
+        self.forms: list[pkmn_form] = list(pkmn_form(x) for x in data['forms'])
+    
+    def find_form(self, variant: str | None) -> pkmn_form | None :
+        return next(filter(lambda x: x.variant == variant, self.forms), None)
+
+
+def load_groups() -> list[pkmn_group] :
+    data = load_json('pokemon.json')
+    # TODO: this is not ideal (should load only once)
+    # but in practice we only load the data once, so it is sufficient
+    schema = load_json('schema.json')
+    jsonschema.validate(data, schema)
+    return list(pkmn_group(x) for x in data)
